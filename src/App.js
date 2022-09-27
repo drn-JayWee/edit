44a7418939a25251ee0652250b760d46d3fa485e
jay wee <mr.jaywee@gmail.com>
2022-09-27 16:36:18 +0300

    27.09.2022

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
+import { useState } from 'react';
 import './App.css';
 
+
 function App(props) {
+
+	const [messegeList, setMessegeList] = useState([
+		'Text',
+		'Author'
+	])
+
+
 	return (
 		<div className="App">
 			<header className="App-header">
-				<div className='cart'>
-					<h3>Hello {props.name}</h3>
-				</div>
-				<div></div>
 			</header>
+			<body>
+				<div className='info'>
+					{
+						messegeList.map(e => <div>{e}</div>)
+					}
+				</div>
+				<div className='new-form'>
+					<form action="">
+						<textarea name="" id=""></textarea><br></br>
+						<button className='form-btn'>OK</button>
+					</form>
+
+				</div>
+			</body>
 		</div>
 	);
 }
 
 export default App;
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ const myName = 'JayWee';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App name = {myName} />
+		<App/>
 	</React.StrictMode>
 );
+
 
 
